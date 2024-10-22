843ec7d18be2bd9e67e9b7c7fbc4663113840016
vikenlakkad970@gmail.com <172198849+Viken-Lakkad@users.noreply.github.com>
2024-10-22 16:23:00 +0530

    Add Arriva lOffer

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// scroll to top button
+
+function toTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// custom cursor
 const cursorDot = document.querySelector(".cursor-dot");
 const cursorOutline = document.querySelector(".cursor-outline");
 
@@ -5,11 +13,9 @@ window.addEventListener("mousemove", function (e) {
   const posX = e.clientX;
   const posY = e.clientY;
 
-  // Move the small cursor dot
   cursorDot.style.left = posX + "px";
   cursorDot.style.top = posY + "px";
 
-  // Animate the outline cursor
   cursorOutline.animate(
     [
       { left: cursorOutline.style.left, top: cursorOutline.style.top },
@@ -18,5 +24,3 @@ window.addEventListener("mousemove", function (e) {
     { duration: 500, fill: "forwards" }
   );
 });
-
-// Add a click event listener to the document
